fcb61eca3ec7725771280a1f5156bc0a3a1b836c
shaomaicheng <devchenglei@gmail.com>
2020-07-05 16:25:51 +0800

    [feature] specify print type when init plugin [功能]初始化的时候指定日志输出类型

--- lib/clogger.dart
+++ lib/clogger.dart
@@ -1,29 +1,36 @@
 import 'package:clogger/logger/base_logger.dart';
 import 'package:clogger/logger/dart_logger.dart';
+import 'package:clogger/logger/platform_logger.dart';
 
 class Clogger {
-
   static Clogger _clogger;
 
-  BaseLogger logger ;
+  BaseLogger logger;
 
-  Clogger._init() {
-  }
+  Clogger._init() {}
 
   factory Clogger() => _inital();
 
-  static Clogger _inital(){
-   if (_clogger == null){
-     _clogger = new Clogger._init();
-   }
-     return _clogger;
+  static Clogger _inital() {
+    if (_clogger == null) {
+      _clogger = new Clogger._init();
+    }
+    return _clogger;
   }
 
-  static Clogger getInstance(){
+  static Clogger getInstance() {
     return _inital();
   }
 
-  init() {
-    logger = DartLogger();
+  init({LogPrintType printType = LogPrintType.dart}) {
+    if (printType == LogPrintType.dart) {
+      logger = DartLogger();
+    } else if (printType == LogPrintType.platform) {
+      logger = PlatformLogger();
+    } else {
+      logger = DartLogger();
+    }
   }
 }
+
+enum LogPrintType { dart, platform }
